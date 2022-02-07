fa1b3d3acbbe253509db5126695316b918e376f1
matheusfacciolla <matheustassifacciolla@gmail.com>
2022-02-06 22:39:00 -0300

    versão atualizada com bônus

--- script.js
+++ script.js
@@ -6,59 +6,63 @@ let srcsFace = ["bobrossparrot",
                 "tripletsparrot", 
                 "unicornparrot"]; 
 
-let listaDeCartas = [];
-let qtdCartas = 0;
+let cardsList = [];
+let qtdCards = 0;
 let firstCard = null;
 let secondCard = null;
 let blockCards = false;
-let ponto = 0;
-let contador = 0;
+let point = 0;
+let counter = 0;
+let interval = null;
+const clock = document.querySelector(".clock");
 
-quantidadesCartas();
+numberOfCards();
 
-function quantidadesCartas(){
-  while (qtdCartas < 4 || qtdCartas > 14 || qtdCartas % 2 !== 0){
-    qtdCartas = parseInt(prompt("Quantas cartas? (4 -> 14)"));
+function numberOfCards(){
+  while (qtdCards < 4 || qtdCards > 14 || qtdCards % 2 !== 0){
+    qtdCards = parseInt(prompt("Quantas cartas? (4 -> 14)"));
   }
-  colocandoCartas(qtdCartas)
+  placingCards(qtdCards)
 }  
 
-function colocandoCartas(qtdCartas){
-  srcsFace.sort(comparador);
+function placingCards(qtdCards){
+  srcsFace.sort(comparator);
 
-  for (let i = 0; i < qtdCartas / 2; i++){
-    listaDeCartas.push(srcsFace[i]);
-    listaDeCartas.push(srcsFace[i]);
+  for (let i = 0; i < qtdCards / 2; i++){
+    cardsList.push(srcsFace[i]);
+    cardsList.push(srcsFace[i]);
   }
 
-  listaDeCartas.sort(comparador);
+  cardsList.sort(comparator);
 
-  let cartas = document.querySelector("ul");
-  for (let i = 0; i < qtdCartas; i++){
-      cartas.innerHTML += 
+  let cardsul = document.querySelector("ul");
+  for (let i = 0; i < qtdCards; i++){
+      cardsul.innerHTML += 
       `
-      <li class="carta" data-identifier="card" data-parrots=${listaDeCartas[i]}>
-          <div class ="verso" data-identifier="back-face">
+      <li class="card" data-parrots=${cardsList[i]} data-identifier="card">
+          <div class ="back" data-identifier="back-face">
               <img src="arquivos-uteis/front.png" alt="Papagaio">
           </div>
           <div class ="face" data-identifier="front-face">
-              <img src="arquivos-uteis/${listaDeCartas[i]}.gif" alt="${listaDeCartas[i]}">
+              <img src="arquivos-uteis/${cardsList[i]}.gif" alt="${cardsList[i]}">
           </div>
       </li>
       `
   }
-  const cards = document.querySelectorAll(".carta");
+  const cards = document.querySelectorAll(".card");
   cards.forEach(card => card.addEventListener('click', flipCard));
+
+  interval = setInterval(increaseCount, 1000);
+  clock.innerHTML = 0;
 } 
 
 function flipCard() {
- 
   if(blockCards === true){
     return false;
   }
 
   this.classList.toggle('flip');
-  contador++;
+  counter++;
 
   if(firstCard === null){
     firstCard = this;
@@ -73,10 +77,13 @@ function checkCards(){
   let isMatch = null;
 
   if(firstCard.dataset.parrots === secondCard.dataset.parrots){
-    ponto++
+    point++
     isMatch = cleanCards();
   }else{
     disableCards();
+  }
+  if(point === qtdCards/2){
+    setTimeout(endOfGame, 1000)
   }
 }
 
@@ -99,6 +106,33 @@ function cleanCards(){
   blockCards = false;
 }
 
-function comparador() { 
+function endOfGame(){
+  clearInterval(interval);
+  msg = prompt(`Você ganhou em ${counter} jogadas e ${clock.innerHTML} segundos!! Gostaria de jogar novamente? (sim) x (não)`);
+  
+  if(msg === "sim"){
+    counter = 0;
+    point = 0;
+    qtdCards = 0;
+    cardsList = [];
+    let cardsul = document.querySelector("ul");
+    cardsul.innerHTML = "";
+    cleanCards();
+    numberOfCards();
+  }else{
+    alert(`Obrigado por jogar!!`)
+  }
+}
+
+function comparator() { 
 	return Math.random() - 0.5; 
 }
+
+function increaseCount() {
+  clock.innerHTML = parseInt(clock.innerHTML) + 1; 
+}
+
+
+ 
+
+
